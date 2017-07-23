e93e98025f5641ebf7b808724a5b46381b650cf9
Jamel Eddine Zarga <jamel.zarga@gmail.com>
2017-07-23 19:16:51 +0100

    Wrap the searchy template

--- public/partials/searchy-tpl.php
+++ public/partials/searchy-tpl.php
@@ -11,57 +11,59 @@
  */
 
 ?>
-<div class="searchy-input-wraper">
-	<input type="text" placeholder="Searchy..." class="searchy-input">
-	<div class="remove"><img src="<?php echo SEARCHY_PLUGIN_URL; ?>/assets/close.png"></div>
-	<div class="spinner" style="display: none;"><img src="<?php echo SEARCHY_PLUGIN_URL; ?>/assets/spinner.gif"></div>
-	<div class="conf"><img src="<?php echo SEARCHY_PLUGIN_URL; ?>/assets/conf.png"></div>
-	<div class="params" style="display: none;">
-		<div class="post_types">
-			<div class="title">Post types</div>
-			<ul>
-				<li>
-					<label>Posts
-						<input type="checkbox" value="post" checked>
-					</label>
-				</li>
-				<li>
-					<label>Pages
-						<input type="checkbox" value="page">
-					</label>
-				</li>
-			</ul>
-		</div>
-		<div class="search_in">
-			<div class="title">Search in</div>
-			<ul>
-				<li>
-					<label>Title
-						<input type="checkbox" value="title">
-					</label>
-				</li>
-				<li>
-					<label>Content
-						<input type="checkbox" value="content" checked>
-					</label>
-				</li>
-				<li>
-					<label>Excerpt
-						<input type="checkbox" value="excerpt">
-					</label>
-				</li>
-			</ul>
-		</div>
+<div class="searchy">
+	<div class="searchy-input-wraper">
+		<input type="text" placeholder="Searchy..." class="searchy-input">
+		<div class="remove"><img src="<?php echo SEARCHY_PLUGIN_URL; ?>/assets/close.png"></div>
+		<div class="spinner" style="display: none;"><img src="<?php echo SEARCHY_PLUGIN_URL; ?>/assets/spinner.gif"></div>
+		<div class="conf"><img src="<?php echo SEARCHY_PLUGIN_URL; ?>/assets/conf.png"></div>
+		<div class="params" style="display: none;">
+			<div class="post_types">
+				<div class="title">Post types</div>
+				<ul>
+					<li>
+						<label>Posts
+							<input type="checkbox" value="post" checked>
+						</label>
+					</li>
+					<li>
+						<label>Pages
+							<input type="checkbox" value="page">
+						</label>
+					</li>
+				</ul>
+			</div>
+			<div class="search_in">
+				<div class="title">Search in</div>
+				<ul>
+					<li>
+						<label>Title
+							<input type="checkbox" value="title">
+						</label>
+					</li>
+					<li>
+						<label>Content
+							<input type="checkbox" value="content" checked>
+						</label>
+					</li>
+					<li>
+						<label>Excerpt
+							<input type="checkbox" value="excerpt">
+						</label>
+					</li>
+				</ul>
+			</div>
 
-		<div class="meta">
-			<div class="title">Custom field</div>
-			<label style="display: inline-block; width: 49%;">Key<input type="text" class="query_meta_key"></label>
-			<label style="display: inline-block; width: 49%;">Value<input type="text" class="query_meta_value"></label>
+			<div class="meta">
+				<div class="title">Custom field</div>
+				<label style="display: inline-block; width: 49%;">Key<input type="text" class="query_meta_key"></label>
+				<label style="display: inline-block; width: 49%;">Value<input type="text" class="query_meta_value"></label>
+			</div>
 		</div>
 	</div>
-</div>
-<div class="searchy-results">
-	<div class="title">Results go here..</div>
-	<div class="content"></div>
+	<div class="searchy-results">
+		<div class="title">Results go here..</div>
+		<div class="content"></div>
+	</div>
 </div>
 <?php
