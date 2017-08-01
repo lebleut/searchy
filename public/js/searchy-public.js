628da96354213357f78f7c89cfb8c302857e396c
Jamel Eddine Zarga <jamel.zarga@gmail.com>
2017-08-01 06:55:49 +0100

    Fix Bug make each searchy instance work seperately

--- public/js/searchy-public.js
+++ public/js/searchy-public.js
@@ -39,46 +39,51 @@
 
 	$( window ).load(function() {
 		// Icons style depending on the main searchy input
-		jQuery(".searchy .icon").each(function(index){
-			var searchyInputHeight = jQuery(this).parents(".searchy").first().find(".searchy-input").innerHeight();
+		$(".searchy .icon").each(function(index){
+			var searchyInputHeight = $(this).parents(".searchy").first().find(".searchy-input").innerHeight();
 
 			var padding = 5*searchyInputHeight/100;
 
 			// Width
-			jQuery(this).innerWidth( searchyInputHeight - (2 * padding) );
-			jQuery(this).find(".dashicons").css("font-size", searchyInputHeight - (2 * padding) );
+			$(this).innerWidth( searchyInputHeight - (2 * padding) );
+			$(this).find(".dashicons").css("font-size", searchyInputHeight - (2 * padding) );
 
 			// Top position
-			jQuery(this).css("top",padding);
+			$(this).css("top",padding);
 
 			// Right position
-			if( jQuery(this).hasClass("spinner") || jQuery(this).hasClass("remove") ){
-				jQuery(this).css("right", searchyInputHeight + padding);
+			if( $(this).hasClass("spinner") || $(this).hasClass("remove") ){
+				$(this).css("right", searchyInputHeight + padding);
 			}else{
-				jQuery(this).css("right",padding);
+				$(this).css("right",padding);
 			}
 		});
 
 		// Behaviour
-		$( ".searchy-input-wraper input[type=text]" ).live("input", function( event ){ searchyHandler(event); });
-		$( ".searchy-input-wraper input[type=checkbox]" ).live("change", function( event ){ searchyHandler(event); });
+		$( ".searchy-input-wraper input[type=text]" ).live("input", function( event ){ searchyHandler( event, $(this) ); });
+		$( ".searchy-input-wraper input[type=checkbox]" ).live("change", function( event ){ searchyHandler( event, $(this) ); });
 		$(".searchy-input-wraper .conf").live("click", function( event ){
-			$(".searchy-input-wraper .params").toggle();
+			var $searchyParent = $(this).parents(".searchy").first();
+
+			$searchyParent.find(".searchy-input-wraper .params").toggle();
 		});
 		$(".searchy-input-wraper .remove").live("click", function( event ){
-			$(".searchy-input-wraper input.searchy-input").val("");
-			$(".searchy-input-wraper input.searchy-input").trigger("input");
+			var $searchyParent = $(this).parents(".searchy").first();
+
+			$searchyParent.find(".searchy-input-wraper  input.searchy-input").val("");
+			$searchyParent.find(".searchy-input-wraper  input.searchy-input").trigger("input");
 		});
 	});
 
-	function searchyHandler( event ){
-		var $searchyQuery = $(".searchy-input-wraper input.searchy-input").val();
+	function searchyHandler( event, currentSearchy ){
+		var $searchyParent = currentSearchy.parents(".searchy").first();
+		var $searchyQuery = $searchyParent.find("input.searchy-input").val();
 
 	    clearTimeout(searchyIdleTimer);
             
         if (searchyIdleState == true) {
         	// Clear results
-        	$( ".searchy-results .content" ).html( "" );
+        	$searchyParent.find( ".searchy-results .content" ).html( "" );
 
             // TODO : remove old AJAX request if exists here
 
@@ -89,19 +94,19 @@
         if( $searchyQuery != "" ){
             searchyIdleTimer = setTimeout(
             	function(){
-            		searchyQueryRequest( $searchyQuery );
+            		searchyQueryRequest( $searchyQuery, $searchyParent );
             	},
             	searchyIdleWait
             );
         }else{
-			$( ".searchy-input-wraper .remove" ).hide();
+			$searchyParent.find( ".remove" ).hide();
         }
 
 	}
-	function searchyQueryRequest( query ){
+	function searchyQueryRequest( query, searchyParent ){
 		// Idle Event : Ajax request here
-		$( ".searchy-input-wraper .spinner" ).show();
-		$( ".searchy-input-wraper .remove" ).hide();
+		searchyParent.find( ".spinner" ).show();
+		searchyParent.find( ".remove" ).hide();
 
 		if(searchyXhr && searchyXhr.readystate != 4){
 			// Abord previous ajax call if new input request 
@@ -110,13 +115,13 @@
 
 		var query_post_types = [];
 
-		$('.post_types input:checked').each(function(index){
+		searchyParent.find('.post_types input:checked').each(function(index){
 			query_post_types.push($(this).val());
 		});
 
 		var query_search_in = [];
 
-		$('.search_in input:checked').each(function(index){
+		searchyParent.find('.search_in input:checked').each(function(index){
 			query_search_in.push($(this).val());
 		});
 		
@@ -127,17 +132,17 @@
 		        'query_text': query,
 		        'query_post_types': query_post_types,
 		        'query_search_in': query_search_in,
-		        'query_meta_key': $('.query_meta_key').val(),
-		        'query_meta_value': $('.query_meta_value').val()
+		        'query_meta_key': searchyParent.find('.query_meta_key').val(),
+		        'query_meta_value': searchyParent.find('.query_meta_value').val()
 		    },
 		    function( response ){
 		    	if( response == 0 ){
 		    		response = "No result!";
 		    	}
 		    	// Show results
-				$( ".searchy-results .content" ).html( response );
-				$( ".searchy-input-wraper .spinner" ).hide();
-				$( ".searchy-input-wraper .remove" ).show();
+				searchyParent.find( ".searchy-results .content" ).html( response );
+				searchyParent.find( ".spinner" ).hide();
+				searchyParent.find( ".remove" ).show();
 		    }
 		);
 
